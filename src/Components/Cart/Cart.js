7cdcb7e9d6e89f55f81bcf4f58fa95107a1cd168
MohammadNasir520 <nasirahsan520@gmail.com>
2022-09-26 19:26:44 +0600

    shop folder move to components

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = ({cart}) => {
     console.log(cart)
     return (
-        <div>
+        <div className='cart'>
             <h2> Order Summery</h2>
             <p>Selected cart: {cart.length}</p>
+            <p>total price:</p>
+            <p>Total Shipping :</p>
+            <h5>Grand total:</h5>
         </div>
     );
 };
